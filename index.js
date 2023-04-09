c039e8e9da6b0777a1ad797684957276f74dff60
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-04-09 14:56:16 +0200

    branch from mustafa

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ obj = {
         name: "Ibrahim",
         printname: function() {
                 console.log(this.name)
+                console.log("hello world from Mustafa")
         }
 }
 obj.printname()
